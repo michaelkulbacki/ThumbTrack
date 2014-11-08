e34d8fed4084918bc8268301639e6fd165ac490b
ajkirschner <ajkirschner16@gmail.com>
2014-11-08 02:34:59 -0500

    Revert "Start and end!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
    This reverts commit 55450450d0cfcf6ec97faa77d83c7dc0e19b0485.
    
    Conflicts:
    	MyApplication/app/src/main/java/ubhacking/thumbpedometer/Touchable.java

--- MyApplication/app/src/main/java/ubhacking/thumbpedometer/Touchable.java
+++ MyApplication/app/src/main/java/ubhacking/thumbpedometer/Touchable.java
@@ -8,11 +8,10 @@ import android.view.View;
  */
 public class Touchable implements View.OnTouchListener{
     private MotionEvent.PointerCoords _event;
-    private Data _data;
-    public Touchable(Data d){
+
+    public Touchable(){
         super();
-        _data=d;
-//        System.out.println("Touchable Created");
+        System.out.println("Touchable Created");
     }
 
     //Calculates total distance of motion _event
@@ -37,44 +36,24 @@ public class Touchable implements View.OnTouchListener{
 
     @Override
     public boolean onTouch(View view, MotionEvent motionEvent){
-        //System.out.println(motionEvent.getAction());
-        float xInit=0, yInit=0, xFinal=0, yFinal=0;
 
-        if(motionEvent.getAction() == MotionEvent.ACTION_DOWN){
-//            System.out.println("Touch Start");
-            xInit=motionEvent.getRawX();
-            yInit=motionEvent.getRawY();
-//            System.out.println("Raw x: "+motionEvent.getRawX());
-//            System.out.println("Raw y: "+motionEvent.getRawY());
-        }
-        else if(motionEvent.getAction() == MotionEvent.ACTION_UP){
-//            System.out.println("Touch end"+'\n');
-            xFinal= motionEvent.getRawX();
-            yFinal = motionEvent.getRawY();
-//            System.out.println("Raw x: "+motionEvent.getRawX());
-//            System.out.println("Raw y: "+motionEvent.getRawY());
-        }
-        _data.setX(xFinal-xInit);
-        _data.setY(yFinal-yInit);
-        _data.setTotalDist(xFinal-xInit,yFinal-yInit);
+        switch(motionEvent.getAction()){
 
-//        switch(motionEvent.getAction()){
-//
-//            case MotionEvent.ACTION_DOWN : {
-//                System.out.println("Touch Start");
-//                System.out.println("Raw x: "+motionEvent.getRawX());
-//                System.out.println("Raw y: "+motionEvent.getRawY());
-//            }
-//            case MotionEvent.ACTION_UP : {
-//                System.out.println("Touch end");
-//                System.out.println("Raw x: "+motionEvent.getRawX());
-//                System.out.println("Raw y: "+motionEvent.getRawY());
-//            }
-//        }
+            case MotionEvent.ACTION_DOWN : {
+                System.out.println("Touch Start");
+                System.out.println("Raw x: "+motionEvent.getRawX());
+                System.out.println("Raw y: "+motionEvent.getRawY());
+            }
+            case MotionEvent.ACTION_UP : {
+                System.out.println("Touch end");
+                System.out.println("Raw x: "+motionEvent.getRawX());
+                System.out.println("Raw y: "+motionEvent.getRawY());
+            }
+        }
 
 //        System.out.println("getX: "+motionEvent.getX());
 //        System.out.println("getY: "+motionEvent.getY());
-       // System.out.println("Touch complete");
+        System.out.println("Touch complete");
 
         return true;
     }
