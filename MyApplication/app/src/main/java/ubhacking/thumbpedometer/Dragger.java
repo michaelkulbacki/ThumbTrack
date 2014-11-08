472dfacd88ff1a2efa4d2883e1e8509550dd0713
John McKissick <johnmiv94@stny.rr.com>
2014-11-08 01:03:28 -0500

    Adds Data class, as well as Touchable class.  Data holds data points.  Touchable creates new motion event

--- MyApplication/app/src/main/java/ubhacking/thumbpedometer/Dragger.java
+++ MyApplication/app/src/main/java/ubhacking/thumbpedometer/Dragger.java
@@ -7,6 +7,10 @@ import android.view.View;
  * Created by John on 11/8/14.
  */
 public class Dragger implements View.OnDragListener {
+    public Dragger(){
+        super();
+        System.out.println("Dragger Created");
+    }
     @Override
     public boolean onDrag(View view, DragEvent dragEvent) {
         System.out.println("dragged");
