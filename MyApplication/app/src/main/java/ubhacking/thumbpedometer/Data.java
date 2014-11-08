3c3c4efd6f07cd99a51321532b25cf66bfe287bd
John McKissick <johnmiv94@stny.rr.com>
2014-11-07 21:46:33 -0500

    Adds Data class, as well as Touchable class.  Data holds data points.  Touchable creates new motion event

---
+++ MyApplication/app/src/main/java/ubhacking/thumbpedometer/Data.java
@@ -0,0 +1,12 @@
+package ubhacking.thumbpedometer;
+
+/**
+ * Created by John on 11/7/14.
+ */
+public class Data {
+    private int _xDist, _yDist, _totalDist;
+
+    public Data(){
+
+    }
+}
---
+++ MyApplication/app/src/main/java/ubhacking/thumbpedometer/Touchable.java
@@ -0,0 +1,13 @@
+package ubhacking.thumbpedometer;
+
+import android.view.MotionEvent;
+
+/**
+ * Created by John on 11/7/14.
+ */
+public class Touchable {
+    MotionEvent.PointerCoords event;
+    public Touchable(){
+        event
+    }
+}
