7c301cef66bf87e8745728c70d89ece6dff20e09
John McKissick <johnmiv94@stny.rr.com>
2014-11-08 01:45:55 -0500

    Adds Data class, as well as Touchable class.  Data holds data points.  Touchable creates new motion event

--- MyApplication/app/src/main/java/ubhacking/thumbpedometer/Data.java
+++ MyApplication/app/src/main/java/ubhacking/thumbpedometer/Data.java
@@ -10,10 +10,22 @@ public class Data {
 
     }
 
+    //Sets Y dist
+    public void setY(float yChange){
+        _yDist+=Math.abs(yChange);
+        System.out.println("Total y distance: "+_yDist);
+    }
+
     //Set X Dist
     public void setX(float xChange){
 
-        _xDist+=xChange;
-        System.out.println(_xDist);
+        _xDist+=Math.abs(xChange);
+        System.out.println("Total x Distance: "+_xDist);
+    }
+
+    //Set Total distance
+    public void setTotalDist(float xChange, float yChange){
+        _totalDist+=Math.abs((Math.pow(xChange,2)+Math.pow(yChange,2)));
+        System.out.println("Total Distance scrolled: "+_totalDist);
     }
 }
