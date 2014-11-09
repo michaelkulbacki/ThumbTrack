21209b4fa7cad1d537b80257ee6f0384ec5956bc
John McKissick <johnmiv94@stny.rr.com>
2014-11-09 02:50:36 -0500

    Fully functional for in app, except data resets with rotation

--- MyApplication/app/src/main/java/ubhacking/thumbpedometer/Touchable.java
+++ MyApplication/app/src/main/java/ubhacking/thumbpedometer/Touchable.java
@@ -18,23 +18,12 @@ public class Touchable implements View.OnTouchListener{
     public Touchable(Data d){
         super();
         _data=d;
-//        System.out.println("Touchable Created");
     }
 
     //Calculates total distance of motion _event
     public float calcDist(float x, float y){
         return (float)Math.abs((Math.pow(x,2)+Math.pow(y,2)));
     }
-
-    //Gets x component of motion _event
-//    public float getX(){
-//        return _event.x;
-//    }
-
-    //Gets y component of motion _event
-//    public float getY(){
-//        return _event.y;
-//    }
 
     //Gets total dist moved
     public float getTotalDist(){
